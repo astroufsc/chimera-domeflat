ec8f4656caccea5e3eff226fc87015745239ae46
tiago <tiago.astro@gmail.com>
2016-06-25 21:14:42 -0400

    Basic renaming.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='chimera_autodomeflat',
     version='0.0.1',
-    packages=['chimera_autodomeflat', 'chimera_autodomeflat.instruments', 'chimera_autodomeflat.controllers'],
+    packages=['chimera_domeflat', 'chimera_domeflat.instruments', 'chimera_domeflat.controllers'],
     scripts=['scripts/chimera-domeflat'],
     url='',
     license='GPL v2',
