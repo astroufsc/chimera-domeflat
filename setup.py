3507381f7257c64273a9818b11b566fb930bfd68
tiago <tiago.astro@gmail.com>
2016-06-25 21:11:06 -0400

    Implement auto dome flat controller.

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 
 setup(
-    name='chimera_template',
+    name='chimera_autodomeflat',
     version='0.0.1',
-    packages=['chimera_template', 'chimera_template.instruments', 'chimera_template.controllers'],
-    scripts=[],
-    url='http://github.com/astroufsc/chimera-template',
+    packages=['chimera_autodomeflat', 'chimera_autodomeflat.instruments', 'chimera_autodomeflat.controllers'],
+    scripts=['scripts/chimera-domeflat'],
+    url='',
     license='GPL v2',
-    author='William Schoenell',
-    author_email='william@iaa.es',
-    description='Template for chimera plugins'
+    author='Tiago Ribeiro',
+    author_email='tribeiro@ufs.br',
+    description='Auto Dome flat controller.'
 )
