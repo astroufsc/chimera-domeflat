b7d3d1275728d043563fd5ed9965a7b47bee7619
tiago <tiago.astro@gmail.com>
2016-06-25 22:56:05 -0400

    Exclude non-existing "instruments" folder from setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='chimera_domeflat',
     version='0.0.1',
-    packages=['chimera_domeflat', 'chimera_domeflat.instruments', 'chimera_domeflat.controllers'],
+    packages=['chimera_domeflat', 'chimera_domeflat.controllers'],
     scripts=['scripts/chimera-domeflat'],
     url='http://github.com/astroufsc/chimera-domeflat',
     license='GPL v2',
